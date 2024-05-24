aa50d3a02ea8f95b660bf169e7585ad79ebec00f
Royakash2 <yahmeakash@gmail.com>
2024-05-25 05:57:51 +0600

    index.ts format fixing with pretier extention

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 export default {
-    port:process.env.PORT,
-    DB_URL:process.env.DATABASE_URL
-}
+  port: process.env.PORT,
+  DB_URL: process.env.DATABASE_URL,
+};
